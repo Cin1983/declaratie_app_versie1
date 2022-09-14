35f19da4d087801c267fd3fbd033e18d56e450cc
MariaGroeizaam <MariaGroeizaam@DESKTOP-A72GRDE>
2022-09-14 10:34:46 +0200

    Add project files.

---
+++ declaratie_app_versie1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace declaratie_app_versie1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
